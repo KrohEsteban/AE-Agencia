61bdefb1a43fc480da8848c01c16f80a664b206a
Esteban Kroh <krohesteban@gmail.com>
2023-06-10 18:41:16 -0300

    faltaron key en los maps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default function Page() {
             }) => {
               return (
 
-                <Cards
+                <Cards key={item.titulo}
                   imagen={item.imagen}
                   titulo={item.titulo}
                   texto={item.texto}
